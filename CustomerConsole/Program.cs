07cd69b5ab2e6f419c589d6f8548fec3d3c39036
salaga999999999 <salaga999999999@gmail.com>
2025-05-26 14:38:14 +0700

    Add solution and 3 projects

---
+++ CustomerConsole/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace CustomerConsole
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("Press 'A' to add item, 'D' to delete item, 'X' to exit program");
+                var key = Console.ReadKey();
+                switch (key.Key)
+                {
+                    case ConsoleKey.A:
+                        {
+
+                        }
+                        break;
+                        case ConsoleKey.D:
+                        {
+
+                        }
+                        break;
+                        case ConsoleKey.X:
+                        {
+                            Console.WriteLine("Program close success.");
+                            return;
+                        }
+                        break;
+                    default:
+                        break;
+                }
+            }
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ JackConsole/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace JackConsole
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
